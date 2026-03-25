a487975f4469c4748788b4e36481b27951eef7db
Anuj Chaudhary <chaudharyanuj0123@gmail.com>
2026-03-25 15:34:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 // Security
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: `https://vote-pole-1.onrender.com`,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
